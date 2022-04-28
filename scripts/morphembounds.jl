177341a18f7161a11bccfbb4e2572ca7cfbaf17e
Neel Smith <dnsmith.neel@gmail.com>
2022-04-28 11:41:42 -0400

    Add script for splitting up verbs

---
+++ scripts/morphembounds.jl
@@ -0,0 +1,23 @@
+using PolytonicGreek
+const PG = PolytonicGreek
+
+prefixes = readlines("deiknumi-prefixes.txt") .|> PG.nfkc
+disjunction = join(sort(prefixes, by=length, rev=true), "|")
+re = Regex("^($disjunction)(.+)")
+
+function splitem(s)
+    replace(s, re => s"\1-\2")
+end
+
+
+vrb = "προσπαραδίδωμι"
+splitem(vrb)
+
+# Read all morphdata into `v`
+# more than 20,500 omega entries!
+omegas = filter(d -> endswith(d.label,"ω") && isempty(d.itype),  v)
+ostrings = map(d -> d.label, omegas)
+splits = ostrings .|> splitem
+nosplits = filter(s -> !contains(s,"-"), splits)
+# MOre than 7800 have one of the listed prefixes
+filter!(s -> contains(s, "-"), splits)
